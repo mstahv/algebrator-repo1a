33e03a017158ea049674f64e50574fb7c9e3d940
Matti Tahvonen <matti@vaadin.com>
2025-03-13 10:57:38 +0200

    Additional cleanup
    Pulling ENV set PORT in a cleaner way and removed obsolete ServletInitializer

--- src/main/java/com/quizlabs/algebrator/AlgebratorApplication.java
+++ src/main/java/com/quizlabs/algebrator/AlgebratorApplication.java
@@ -1,19 +1,11 @@
 package com.quizlabs.algebrator;
 
-import java.util.Collections;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AlgebratorApplication {
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(AlgebratorApplication.class);
-        app.setDefaultProperties(Collections.singletonMap("server.port", getPort()));
-        app.run(args);
-    }
-
-    private static String getPort() {
-        String port = System.getenv("PORT");
-        return port != null ? port : "8080";
+        SpringApplication.run(AlgebratorApplication.class);
     }
 }
